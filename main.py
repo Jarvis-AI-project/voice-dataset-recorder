a52eade50c734987234d122f1bb35f048fc9ed26
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-12-27 00:19:33 +0530

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 # Provide template folder name
 # The default folder name should be "templates" else need to mention custom folder name
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
- 
+
 # @app.route('/')
 # def welcome():
 #     return "This is the home page of Flask Application"
